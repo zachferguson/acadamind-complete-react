9b0738400bea8c6e2bae7cb1532a32357c3f4e99
zach ferguson <zach.ferguson@yahoo.com>
2022-12-17 21:13:13 -0500

    S3L41 beginning, put expenses logic into a map rather than static

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,30 +3,16 @@ import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 
 let Expenses = (props) => {
-  return (
-    <Card className="expenses">
+  let expenseList = props.expenses.map((expense, i) => {
+    return (
       <ExpenseItem
-        title={props.expenses[0].title}
-        date={props.expenses[0].date}
-        price={props.expenses[0].price}
+        title={expense.title}
+        date={expense.date}
+        price={expense.price}
       />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        date={props.expenses[1].date}
-        price={props.expenses[1].price}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        date={props.expenses[2].date}
-        price={props.expenses[2].price}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        date={props.expenses[3].date}
-        price={props.expenses[3].price}
-      />
-    </Card>
-  );
+    );
+  });
+  return <Card className="expenses">{expenseList}</Card>;
 };
 
 export default Expenses;
